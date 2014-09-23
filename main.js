a2c1eeef2a3f282b65f2d63ec528edd9bf21c7b5
Sarkis Peha <sarkispeha@gmail.com>
2014-09-23 15:55:58 -0600

    finished a few problems

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ var getName = function(obj){
 };
 
 
+var totalLetters = function(arr) {
+	return arr.join('').length;
+};
+
+var keyValue = function(key, value){
+	var obj = {};
+	return obj[key] = value;
+}
+
+var negativeIndex = function(arr, neg) {
+
+	thefucker = (arr.length)+neg;
+	return arr[thefucker];
+}
+
